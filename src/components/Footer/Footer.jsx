cb329fbd3c848e042f6fdeb9ad5875a11f0e0b2c
perli20 <sdinakarbabu111@gmail.com>
2025-01-07 18:45:57 +0530

    Footer created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import './Footer.css';
+const data = [
+    {
+        name:"Quick Links",
+        subList:[
+            {
+                name:"About"
+            },
+            {
+                name:"Contact"
+            }
+        ]
+    },
+    {
+        name:"Products",
+        subList:[
+            {
+                name:"ITR Filing"
+            },
+            {
+                name:"GST Filing"
+            },
+            {
+                name:"Accounting"
+            },
+            {
+                name:"TDS & Tax planning"
+            }
+        ]
+    },
+    {
+        name:"Services For Individuals",
+        subList:[
+            {
+                name:"ITR Filing"
+            },
+            {
+                name:"Tax planning"
+            },
+            {
+                name:"Business consultion"
+            },
+            {
+                name:"HUF Registration"
+            },
+            {
+                name:"Pancard Registration"
+            },
+            {
+                name:"PF & ESI"
+            }
+        ]
+    },
+    {
+        name:"Services For Business",
+        subList:[
+            {
+                name:"ITR Filing"
+            },
+            {
+                name:"GST Filing"
+            },
+            {
+                name:"Budgeting"
+            },
+            {
+                name:"Business Registrations"
+            },
+            {
+                name:"Accounting"
+            },
+            {
+                name:"Financial Planning"
+            }
+        ]
+    }
+]
+
+const Footer = () => {
+  return (
+    <section className='footerSection'>
+        {data.map((each)=>{
+            return(
+                <ul>
+                    <li className='item-name'>{each.name}</li>
+                    <ul>
+                        {each.subList.map((eachInner)=><li className='inner-eachItem'>{eachInner.name}</li>)}
+                    </ul>
+                </ul>
+            )
+        })}
+           
+    </section>
+  )
+}
+
+export default Footer
--- src/components/Greetings/Greetings.jsx
+++ src/components/Greetings/Greetings.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './Greetings.css'
 import Enquiry from '../Enquiry/Enquiry';
+import Footer from '../Footer/Footer';
 const Greetings=()=> {
     const [check,setCheck] = useState(true)
     useEffect(() => {
@@ -12,7 +13,7 @@ const Greetings=()=> {
     return (
         <>
             {(check)?<div className='greetingBox'><h1 className='greetingText'>Welcome !</h1>
-            <p className='greetingsDescription'>Smart Money moves</p></div>:<> <Enquiry /></>}
+            <p className='greetingsDescription'>Smart Money moves</p></div>:<> <Enquiry /> <hr/> <Footer/><hr/></>}
         </>
     )
  
