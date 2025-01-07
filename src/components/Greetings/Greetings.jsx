1b5ab419fada04f7a84591dfa9e5593932ab6b75
perli20 <sdinakarbabu111@gmail.com>
2025-01-07 19:21:38 +0530

    Did you know created

--- src/components/Greetings/Greetings.jsx
+++ src/components/Greetings/Greetings.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './Greetings.css'
 import Enquiry from '../Enquiry/Enquiry';
 import Footer from '../Footer/Footer';
+import DidYouKnow from '../DidYouKnow/DidYouKnow';
 const Greetings=()=> {
     const [check,setCheck] = useState(true)
     useEffect(() => {
@@ -13,7 +14,7 @@ const Greetings=()=> {
     return (
         <>
             {(check)?<div className='greetingBox'><h1 className='greetingText'>Welcome !</h1>
-            <p className='greetingsDescription'>Smart Money moves</p></div>:<> <Enquiry /> <hr/> <Footer/><hr/></>}
+            <p className='greetingsDescription'>Smart Money moves</p></div>:<> <Enquiry /> <DidYouKnow/> <hr/> <Footer/><hr/></>}
         </>
     )
  
