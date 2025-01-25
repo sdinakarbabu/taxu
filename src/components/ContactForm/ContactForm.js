966e0c22ab0c9134668c9bb990145aadcbad6243
Prasanth <Perliedu20@gmail.com>
2025-01-25 19:39:49 +0530

    bootstrap added

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -32,55 +32,72 @@ const ContactForm = (props) => {
     }
   return (
     <section id='ContactFormSection' style={(contactOn)?{zIndex:"100",display:'block'}:{zIndex:"-100",display:'none'}}>
-    <div class="container">
+        <div class="container">
         <div style={{display:"flex", justifyContent:"space-between"}}>
             <h1 class="text"> Contact us Form</h1>
             <button type='button' style={{width:"45px",height:"25px", backgroundColor:"#fff",border:"none", fontSize:"18px"}} onClick={changeState}>Close</button>
         </div>
-        <form onSubmit={onSubmit}>
-            <div class="form-row">
-                <div class="input-data">
-                    <input type="text" name='Firstname' required/>
-                    <div class="underline"></div>
-                    <label for="">First Name</label>
-                </div>
-                <div class="input-data">
-                    <input type="text" name='LastName' required/>
-                    <div class="underline"></div>
-                    <label for="">Last Name</label>
+        <form class="row g-3 needs-validation" novalidate onSubmit={onSubmit}>
+            <div class="col-md-4">
+                <label for="validationCustom01" class="form-label">First name</label>
+                <input type="text" class="form-control" name='Firstname' id="validationCustom01" required/>
+                <div class="valid-feedback">
+                Looks good!
                 </div>
             </div>
-            <div class="form-row">
-                <div class="input-data">
-                    <input type="text" name='Email' required/>
-                    <div class="underline"></div>
-                    <label for="">Email Address</label>
-                </div>
-                <div class="input-data">
-                    <input type="text" name='PhoneNumber' required/>
-                    <div class="underline"></div>
-                    <label for="">Phone Number</label>
+            <div class="col-md-4">
+                <label for="validationCustom02" class="form-label">Last name</label>
+                <input type="text" class="form-control" name='LastName' id="validationCustom02" required/>
+                <div class="valid-feedback">
+                Looks good!
                 </div>
             </div>
-            <select className='Language' name="Language">
-                <option value="English">English</option>
+            <div class="col-md-6">
+                <label for="validationCustomUsername" class="form-label">Email</label>
+                <div class="input-group has-validation">
+                <input type="email" class="form-control" name='Email' id="validationCustomUsername" aria-describedby="inputGroupPrepend" required/>
+                <div class="invalid-feedback">
+                    Please choose a username.
+                </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <label for="validationCustom04" class="form-label">Language</label>
+                <select class="form-select" name="Language" id="validationCustom04" required>
+                <option value="English" selected>English</option>
                 <option value="Telugu">Telugu</option>
                 <option value="Hindi">Hindi</option>
-            </select>
-            <div class="form-row">
-                <div class="input-data textarea">
-                    <textarea rows="8" cols="80" name='message' required></textarea>
-                    <br />
-                    <div class="underline"></div>
-                    <label for="">Write your message</label>
-                    <br />
+                </select>
+                <div class="invalid-feedback">
+                    Please select Language.
                 </div>
-                <button type='submit' className='submit-btn' >Submit</button>
+            </div>
+            <div class="col-md-3">
+                <label for="validationCustom05" class="form-label">Zip</label>
+                <input type="text" class="form-control" id="validationCustom05" required/>
+                <div class="invalid-feedback">
+                    Please provide a valid zip.
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required/>
+                <label class="form-check-label" for="invalidCheck">
+                    Agree to terms and conditions
+                </label>
+                <div class="invalid-feedback">
+                    You must agree before submitting.
+                </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <button class="btn btn-primary" type="submit">Submit form</button>
             </div>
         </form>
-    </div>
-    <p>{result}</p>
-</section>
+        <p>{result}</p>
+        </div>
+    </section>
+
   )
 }
 
