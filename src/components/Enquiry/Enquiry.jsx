30a893765ff2817307cab4b48b5bdea8fc173456
Prasanth <Perliedu20@gmail.com>
2025-02-04 22:04:58 +0530

    contact Page modified with contact us close btn modified

--- src/components/Enquiry/Enquiry.jsx
+++ src/components/Enquiry/Enquiry.jsx
@@ -31,24 +31,6 @@ const Enquiry = (props) => {
     <>
     <section className='EnquirySection' id='EnquirySection'>
       <h1 className='mainQuoteHeading'>"Simplify Your Finances with Expert Tax Filing and Accounting Services."</h1>
-        {/* <div className='EnquiryContainer'>
-          <div className='enquiryBlock'>
-            <img src={logo} alt={logo} className='img-expert'/>
-             <div className='flex-aliginleft'>
-                <h1 className='enquiryHeading'>Lets connect with our expert</h1>
-                <p className='enquiryDescription'>Streamline your finances with professional expertise, ensuring accuracy and compliance while you focus on what matters most.</p>
-                <button className='BtnEnquiry' type='button' onClick={changeState}>Connect   ➜</button>
-              </div>
-          </div>
-          <div className='enquiryBlock'>
-            <img src={Itr} alt={Itr} className='img-expert img-expert2'/>
-              <div className='flex-aliginleft2'>
-                <h1 className='enquiryHeading enquiryHeading2'>Start Exploring Services/Products</h1>
-                <p className='enquiryDescription enquiryDescription2'>Begin your journey towards financial freedom with our comprehensive offerings tailored just for you.</p>
-                <button className='BtnEnquiry' onClick={()=>window.location.href="/products"}>Explore   ➜</button>
-              </div>
-          </div>
-        </div> */}
       <div className='accountingServicesContainer'>
         {
           AccountingData.map((ele)=>{
