3616046093471c46049752f76bd9beb2bb842018
Prasanth <Perliedu20@gmail.com>
2025-02-05 21:32:18 +0530

    interrestingfacts heading added border

--- src/components/WorkFlowTaxu/WorkFlowTaxu.jsx
+++ src/components/WorkFlowTaxu/WorkFlowTaxu.jsx
@@ -35,7 +35,7 @@ const WorkFlowTaxu = () => {
   return (
     <section className='WorkFlowTaxuSection' id='WorkFlowTaxuSection'>
         <h1 className='workFlowHeading'>How It works</h1>
-        <p className='workFlowParagraph'>4 Simple-steps to e-file your <span>Income Tax Returns</span></p>
+        <p className='workFlowParagraph' style={{display:'none'}}>4 Simple-steps to e-file your <span>Income Tax Returns</span></p>
         <div className='flowOfProcessBlock'>
             <ul className='workFlowContainer'>
                {
