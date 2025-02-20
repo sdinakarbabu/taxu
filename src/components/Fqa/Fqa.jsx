64b015794794232e1de608a0327644da9cc2c28e
Prasanth <Perliedu20@gmail.com>
2025-02-20 19:29:58 +0530

    Added changes to FQA and Payment details

--- src/components/Fqa/Fqa.jsx
+++ src/components/Fqa/Fqa.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import downArrow from './downArrow.png'
 import './Fqa.css'
 
 const Fqa = (props) => {
@@ -15,7 +16,7 @@ const Fqa = (props) => {
     <li className='dropdownEachBlock'>
         <div className='questionBLock' onClick={displayAnswer}>
             <p style={{textAlign:'left',padding:'10px 0 0 15px'}}>{ques}</p>
-            <p className='arrowMark'>></p>
+            <img src={downArrow} alt='down-arrow' className='arrowMark' style={(answer)?{transform: 'rotate(0deg)'}: {transform: 'rotate(180deg)'}} />
         </div>
         <div className='answerBlock' style={(answer)?displayNone:{}}> 
             <p>{ans}</p>
